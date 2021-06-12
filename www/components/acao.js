80c8a9a6630e2ed27b2de4fc900d89406708839f
david042 <david.mal04@hotmail.com>
2021-06-13 06:27:28 +0900

    agora ta chave

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,6 +1,8 @@
 window.onload = function(){
   const buscar = document.querySelector("#buscar");
 
+  const cep = document.querySelector("#cep");
+
   const opcoes = {
     method: "GET",
     mode: "cors",
@@ -8,10 +10,13 @@ window.onload = function(){
   }
 
   buscar.addEventListener("click",function(){
-    fetch(`https://viacep.com.br/ws/01001000/json/`, opcoes)
+    fetch(`https://viacep.com.br/ws/${cep.value}/json/`, opcoes)
       .then(response => {response.json()
         .then(data => {
-          alert(`Logradouro: ${data["logradouro"]}`);
+          document.querySelector("#estado").value = data["uf"];
+          document.querySelector("#cidade").value = data["localidade"];
+          document.querySelector("#bairro").value = data["bairro"];
+          document.querySelector("#rua").value = data["logradouro"];
         })
       })
   });
